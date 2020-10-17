bb303555506107005146d1c02e7d6db771304507
wajdiferjani <wajdi.ferjani@esprit.tn>
2020-10-17 12:25:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Profile from './profile/Profie';
+import papillon from './papillon.jpg';
+
 
 function App() {
+  const data = {
+    fullName: 8,
+    bio:'bonjour',
+    profession:'marahbe'
+   
+  }
+  const alertMyInput = fullName => alert(fullName);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Profile alertMyInput={alertMyInput} fullName={data.fullName}  bio={data.bio} profession={data.profession}>{papillon}</Profile>
+      
+
     </div>
   );
 }
